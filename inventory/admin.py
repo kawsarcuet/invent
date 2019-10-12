9ebe3a2cb31c3f52c3396efb8bbb31e2f00358ec
kawsarcuet <jobmail.cuet13@gmail.com>
2019-10-12 11:11:47 +0600

    hello kawsar

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,7 +3,26 @@ from import_export.admin import ImportExportModelAdmin
 from .models import *
 # Register your models here.
 
-# admin.site.register(item)
+# admin.site.register(item) is done
 @admin.register(Desktops, Laptops, Mobiles)
 class ViewAdmin(ImportExportModelAdmin):
     exclude = ('id', )
+              hello my
+
+              i am lucky to go here
+hg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
